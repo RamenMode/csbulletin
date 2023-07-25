8895f4273f60bbbe51c06df1d81b8ce56b966665
Kevin Xue <xuek2022@outlook.com>
2023-07-25 15:11:33 -0400

    fixing cookie consent

--- src/components/Artcs.js
+++ src/components/Artcs.js
@@ -42,14 +42,14 @@ function Artcs() {
       </div>
       <CookieConsent
         location="bottom"
-        buttonText="Sure man!!"
+        buttonText="Ok"
         cookieName="myAwesomeCookieName2"
         style={{ background: "#2B373B" }}
         buttonStyle={{ color: "#4e503b", fontSize: "13px" }}
         expires={150}
         >
         This website uses cookies to enhance the user experience.{" "}
-        <span style={{ fontSize: "10px" }}>This bit of text is smaller :O</span>
+        <span style={{ fontSize: "10px" }}>By contiuning to use this service, you are consenting to the use of cookies</span>
       </CookieConsent>
     </div>
     </>
